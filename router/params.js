0ff940575eaefe7761f7f9e1625fe1607bfb39b1
Roddy Andrade <idarkmer@gmail.com>
2023-01-10 22:05:36 -0500

    Get param and process by description and id

--- router/params.js
+++ router/params.js
@@ -1,9 +1,60 @@
 const router = require('express').Router();
 const {Param} = require('../config/config');
+const { Op } = require("sequelize");
 
 router.get('/', async (req, res) => {
     const params = await Param.findAll();
     res.json(params);
+})
+
+router.get('/byDescription', async (req, res) => {
+    const param = await Param.findAll({
+        where: {
+            description: {
+                [Op.like]: '%'+req.body.param+'%'
+            }
+        }
+    });
+    if (param) {
+        res.json(param);
+    } else {
+        res.send('No se encontró el dato' + req.body)
+    }
+})
+
+router.get('/byField', async (req, res) => {
+    const param = await Param.findAll({
+        where: {
+            field: {
+                [Op.like]: '%'+req.body.field+'%'
+            }
+        }
+    });
+    if (param) {
+        res.json(param);
+    } else {
+        res.send('No se encontró el dato' + req.body)
+    }
+})
+
+router.get('/byFieldAndDescription', async (req, res) => {
+    const param = await Param.findAll({
+        where: {
+            description: {
+                [Op.like]: '%'+req.body.param+'%'
+            },
+            [Op.and] : [
+                {
+                    [Op.like]: '%'+req.body.field+'%'
+                }
+            ]
+        }
+    });
+    if (param) {
+        res.json(param);
+    } else {
+        res.send('No se encontró el dato' + req.body)
+    }
 })
 
 module.exports = router;
--- router/processes.js
+++ router/processes.js
@@ -4,6 +4,6 @@ const {Process} = require('../config/config');
 router.get('/', async (req, res) => {
     const process = await Process.findAll();
     res.json(process);
-})
+});
 
 module.exports = router;
