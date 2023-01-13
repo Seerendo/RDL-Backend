81e62923663834426ff720966d7ec8c501623a6f
Seerendo <70487872+Seerendo@users.noreply.github.com>
2023-01-13 11:46:02 -0500

    Testing DB Mysql Hosting

--- config/config.js
+++ config/config.js
@@ -3,10 +3,26 @@ const Sequelize = require('sequelize');
 const ParamsModel = require('../models/params');
 const ProcessModel = require('../models/processes');
 
-const sequelize = new Sequelize('rdl', 'root', '1234', {
+/* const sequelize = new Sequelize('rdl', 'root', '1234', {
   host: 'localhost',
   dialect: 'mysql',
-});
+}); */
+
+//Prueba de BD Server
+const sequelize = new Sequelize(
+  'rdl',
+  '1ud2si3zh3dnz4gk5cq2',
+  'pscale_pw_CVOS72DCFvOeOM2QAzOk6TAce2GvnEkcADSXOPCjPOD',
+  {
+    host: 'us-east.connect.psdb.cloud',
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    },
+  }
+);
 
 const Param = ParamsModel(sequelize, Sequelize);
 const Process = ProcessModel(sequelize, Sequelize);
